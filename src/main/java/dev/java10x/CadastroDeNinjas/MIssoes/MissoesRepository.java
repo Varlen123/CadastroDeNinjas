25177fd1068ba33bdc1c9faa963d291b7250ce1a
Varlen123 <varlengaspar171@gmail.com>
2025-07-11 01:31:58 -0300

    feat:Adiciona a interface MissoesRepository que extende JPA

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesRepository.java
@@ -0,0 +1,8 @@
+package dev.java10x.CadastroDeNinjas.MIssoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long>{
+
+    
+}
