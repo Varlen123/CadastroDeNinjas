010f2d26b5cca42e984f475d87c02e1ed20d9569
Varlen123 <varlengaspar171@gmail.com>
2025-07-13 10:46:38 -0300

    feat: Altera a rota to controller para listar os ninjas e injeta a dependência do SERVICE e do REPOSITORY

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,51 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
-@RequestMapping
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/ninjas")
 @RestController
 public class NinjaController {
+
+    @Autowired
+    private NinjaService ninjaService;
 
     @GetMapping("/mensagem")
     public String boasVindas(){
         return "Essa é minha primeira mensagem";
+    }
+
+    //Adicionar ninja(CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "ninja criado";
+    }
+    //Buscar ninja por id(READ)
+
+      @GetMapping("/listar")
+        public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
+    }
+    
+
+    //Mostrar todos os ninjas (READ)
+
+     @GetMapping("/todosporId")
+    public String mostrarTodosOsNinjasPorId(){
+        return "id dos ninjas";
+    }
+    
+    //Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjasPorID(){
+        return "Atualizar ninjas por ID";
+    }
+    //Deletar ninja (DELETE)
+
+    @DeleteMapping("/deletar")
+    public String deletarNinjaPorId(){
+        return "Ninja deletado";
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,5 +1,21 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NinjaService {
     
+    @Autowired //inicializa um construtor 
+    private NinjaRepository ninjaRepository;
+
+    //Listar todos os meus ninjas
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+   
 }
