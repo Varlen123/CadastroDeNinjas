7d9e1b684b59b213b5180d43e915c9d5873bc660
Varlen123 <varlengaspar171@gmail.com>
2025-07-15 18:29:57 -0300

    fix: Inclui a coluna de rank e img_url na minha entidade. Porém, já estavam incluidos no meu V3 de migrations

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,34 +1,48 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
-
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-// Entity trasnforma uma classe em uma entidade do banco de dados
+import lombok.ToString;
+
 @Entity
+@Table(name = "tb_cadastro")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "tb_cadastro")
+@ToString(exclude = "missoes")
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column (name = "id")
+    private long id;
 
-    @Column(name = "nome")
+    @Column (name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
-    
-    @Column(name = "idade")
+
+    @Column (name = "idade")
     private int idade;
-    
-    
+
+    @Column (name = "img_url")
+    private String img_url;
+
+    @Column (name = "rank")
+    private String rank;
+
     @ManyToOne
-    @JoinColumn(name = "missoes_id") //Foreign key
+    @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-    
+
 }
